f076c6d0ccd3974b8e9cceba853b98f84482a72c
sourabh gorule <gorulesourabh@gmail.com>
2023-02-17 22:08:54 +0530

    first commit

---
+++ src/main/java/com/po/PizzaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.po;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrderApplication.class, args);
+	}
+
+}
